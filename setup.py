049bab7de89e40988dc46f90632036c29711e06b
Qznan <summerzynqz@gmail.com>
2020-04-18 17:14:13 +0800

    update pypi version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='QizNLP',
-    version='0.1.1',
+    version='0.1.2',
     author='Qznan',
     author_email='summerzynqz@gmail.com',
     description='Quick run NLP in many task',
